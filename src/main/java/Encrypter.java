a1fef1207b1d422614d4617caf344d698499595b
Bernard O'Connor <bernard.oconnor47@mail.dcu.ie>
2017-10-21 16:23:18 +0100

    Setup Project Structure

---
+++ src/main/java/Encrypter.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import java.math.BigInteger;
+import java.security.*;
+import javax.crypto.*;
+
+public class Encrypter {
+
+
+    public static void main(String [] args) {
+    System.out.println("Hello World");
+    }
+}
+
