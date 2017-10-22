76ff0115e8726e21fd3edbf5cd1cffb385bc12e5
Bernard O'Connor <bernard.oconnor47@mail.dcu.ie>
2017-10-22 14:30:44 +0100

    Read in utf-8 password

--- src/main/java/Encrypter.java
+++ src/main/java/Encrypter.java
@@ -1,14 +1,32 @@
 package main.java;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.*;
+import java.util.Scanner;
 import javax.crypto.*;
 
 public class Encrypter {
 
 
     public static void main(String [] args) {
-    System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in, "UTF-8");
+        System.out.println("Please enter a password: ");
+        String passwordString = scan.next();
+        try {
+            byte[] p = passwordString.getBytes("UTF-8"); //p = the password in a utf-8 byte array
+            //print out byte array, TODO remove before final submission
+            for (int i = 0; i < p.length; i++){
+                System.out.println(p[i]);
+            }
+        }
+        catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+
+
     }
 }
 
